25b5ff1ae5e9d96e0867c621b18cb541ccd78c89
Karen <krmoyan1997@gmail.com>
2025-02-04 16:00:14 +0400

    First commit of ongoing project

---
+++ ecommerce/account/urls.py
@@ -0,0 +1,26 @@
+#
+from django.urls import path
+#
+from . import views
+
+urlpatterns = [
+
+    path("register", views.register, name="register"),
+
+    # Email verification URL's
+
+    path("email-verification/<str:uidb64>/<str:token>/", views.email_verification, name="email-verification"),
+
+    path("email-verification-sent", views.email_verification_sent, name="email-verification-sent"),
+
+    path("email-verification-success", views.email_verification_success, name="email-verification-success"),
+
+    path("email-verification-failed", views.email_verification_failed, name="email-verification-failed"),
+    
+    path("my-login", views.my_login, name="my-login"),
+
+    path("dashboard", views.dashboard, name="dashboard"),
+
+    path("user-logout", views.user_logout, name="user-logout"),
+
+]
